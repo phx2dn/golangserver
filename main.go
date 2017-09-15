956d54c865d30f6af2f1b4f108f90b8c836bd718
phx2dn <phx.dn.wang@gmail.com>
2017-09-14 22:42:49 -0700

    first commit

---
+++ main.go
@@ -0,0 +1,44 @@
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"encoding/json"
+	"log"
+)
+
+type Location struct {
+	Lat float64 `json:"lat"`
+	Lon float64 `json:"lon"`
+}
+
+type Post struct {
+	// `json:"user"` is for the json parsing of this User field. Otherwise, by default it's 'User'.
+	User     string `json:"user"`
+	Message  string  `json:"message"`
+	Location Location `json:"location"`
+}
+
+func main() {
+	fmt.Println("started-service")
+	http.HandleFunc("/post", handlerPost)
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+
+func handlerPost(w http.ResponseWriter, r *http.Request) {
+	// Parse from body of request to get a json object.
+	fmt.Println("Received one post request")
+	decoder := json.NewDecoder(r.Body)
+	var p Post
+
+	if err := decoder.Decode(&p); err != nil {
+		panic(err)
+		return
+	}
+
+	fmt.Fprintf(w, "Post received: %s\n", p.Message)
+}
+
+
